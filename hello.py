8a310d6c681187eb8571c420e852d007db3a83f3
ivanopula <phone62f4@gmail.com>
2022-01-17 01:49:48 +0700

    11

--- hello.py
+++ hello.py
@@ -1,9 +1,7 @@
-def wsgi_application(environ, start_response):
-
-     status = '200 OK'
-     headers = [
-         ('Content Type' , 'text/plain')
-     ]
-     body = 'Hello world!'
-     start_response(status , headers)
-     return [body]
+def hello(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+    body = environ['QUERY_STRING']
+    body =( '\n'.join(body.split('&'))+ '\n').encode('utf-8')
+    start_response(status, headers)
+    return body

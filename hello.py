78ed9547fabb0692b78dad819e521b8227a129e4
ivanopula <phone62f4@gmail.com>
2022-02-03 11:15:50 +0700

    13

--- hello.py
+++ hello.py
@@ -2,6 +2,6 @@ def hello(environ, start_response):
     status = '200 OK'
     headers = [('Content-Type', 'text/plain')]
     body = environ['QUERY_STRING']
-    body =( '\n'.join(body.split('&'))+ '\n').encode('utf-8')
+    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     start_response(status, headers)
     return body

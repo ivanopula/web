57858529634a304c452bf842cba622892304de76
ivanopula <phone62f4@gmail.com>
2022-02-12 20:22:27 +0700

    urls

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",views.test),
+    path('question/<int:id>, views.test'),
+    path('login/, views.test'),
+    path('signup/, views.test'),
+    path('ask/, views.test'),
+    path('popular/, views.test'),
+    path('new/, views.test'),
+
 ]
